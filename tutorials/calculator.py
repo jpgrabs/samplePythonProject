1ece0692fe44317c93dea08f37f2721694000019
jpgrabs <jpgrabador@gmail.com>
2022-07-28 19:01:57 +0800

    Building a Basic Calculator

---
+++ tutorials/calculator.py
@@ -0,0 +1,10 @@
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result1 = num1 + num2  # combine only two string
+print(result1)
+
+# result2 = int(num1) + int(num2)  # convert string into integers or whole number only
+# print(result2)
+
+result3 = float(num1) + float(num2)  # convert string into decimal
+print(result3)
