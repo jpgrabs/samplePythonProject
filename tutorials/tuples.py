3700d26bd60034d4f3dfc660ee1e04d71b6a60c2
jpgrabs <jpgrabador@gmail.com>
2022-07-28 21:52:51 +0800

    tuples and basic function

---
+++ tutorials/tuples.py
@@ -0,0 +1,35 @@
+# Tuples is a type of data structure which basically means container where can store different value
+# Tuples is very similar to list top store multiple information in the list but different in lift function
+
+coordinates = (4, 5)  # can't change and don't modify the list
+print(coordinates[0])  # select specific index
+
+# Basic Function Tutorial
+# coordinates[1] = 10  # TypeError: 'tuple' object does not support item assignment
+# print(coordinates)  # tuples is immutable because you can't change the list
+
+# def means function in python, the function must be inside the indent funtion
+def hello_world():
+    print("Hello user")
+    your_feeling = input("Are you feeling well today? : ")
+    print(your_feeling + ", Wow that's great!")
+
+
+print("--------")
+hello_world()
+print("--------")
+
+
+def the_function(name, age):
+    print("Hello " + name + " and you are " + age)
+
+
+the_function("Python", "27")
+the_function("Android", "29")
+
+
+def the_function2(name, age):
+    print("And also hello " + name + " and you are " + str(age))
+
+
+the_function2("IOs", 11)
