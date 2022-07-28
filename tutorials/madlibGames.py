18697338ea7ecaaa01de0a4d73473214405d9ef5
jpgrabs <jpgrabador@gmail.com>
2022-07-28 20:47:06 +0800

    madlib games

---
+++ tutorials/madlibGames.py
@@ -0,0 +1,11 @@
+name = input("Enter your name: ")
+print("You are " + name)
+
+color = input("Enter your hair color: ")
+print(color + " is your hair color")
+
+occupation = input("Enter your occupation: ")
+print("Your occupation is " + occupation + " and that's awesome!")
+
+
+
