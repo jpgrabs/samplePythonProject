1089a57a20210c02187a2d1ad0fe756df8142a70
jpgrabs <jpgrabador@gmail.com>
2022-08-04 22:42:14 +0800

    Building a better Calculator

--- tutorials/calculator.py
+++ tutorials/calculator.py
@@ -1,10 +1,27 @@
-num1 = input("Enter a number: ")
-num2 = input("Enter another number: ")
-result1 = num1 + num2  # combine only two string
-print(result1)
+# num1 = input("Enter a number: ")
+# num2 = input("Enter another number: ")
+# result1 = num1 + num2  # combine only two string
+# print(result1)
+#
+# # result2 = int(num1) + int(num2)  # convert string into integers or whole number only
+# # print(result2)
+#
+# result3 = float(num1) + float(num2)  # convert string into decimal
+# print(result3)
 
-# result2 = int(num1) + int(num2)  # convert string into integers or whole number only
-# print(result2)
+# Building a better calculator
+num1 = float(input("Enter 1st number: "))
+operator = input("Enter operator: ")
+num2 = float(input("Enter 2nd number: "))
 
-result3 = float(num1) + float(num2)  # convert string into decimal
-print(result3)
+if operator == "+":
+    print(num1 + num2)
+elif operator == "-":
+    print(num1 - num2)
+elif operator == "*":
+    print(num1 * num2)
+elif operator == "/":
+    print(num1 / num2)
+else:
+    print("Invalid operator")
+
