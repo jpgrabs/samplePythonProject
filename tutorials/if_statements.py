d5e3070db9b163864089db46df033ee0f8b76689
jpgrabs <jpgrabador@gmail.com>
2022-08-04 22:06:05 +0800

    If Statements & Comparisons

--- tutorials/if_statements.py
+++ tutorials/if_statements.py
@@ -1,4 +1,3 @@
-
 # is_male = True
 # is_tall = False
 #
@@ -7,14 +6,49 @@
 # else:
 #     print("You neither Male nor tall")
 
-is_male1 = False
-is_tall1 = True
+# is_male1 = False
+# is_tall1 = True
+#
+# if is_male1 and is_tall1:
+#     print("You are tall male")
+# elif is_male1 and not is_tall1:
+#     print("You are short Male")
+# elif not is_male1 and is_tall1:
+#     print("You are not a Male but tall")
+# else:
+#     print("You are either not male or not tall or both")
 
-if is_male1 and is_tall1:
-    print("You are tall male")
-elif is_male1 and not is_tall1:
-    print("You are short Male")
-elif not is_male1 and is_tall1:
-    print("You are not a Male but tall")
-else:
-    print("You are either not male or not tall or both")
+
+def max_num(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num3 >= num1:
+        return num2
+    else:
+        return num3
+
+
+input_num1 = input("Enter 1st num: ")
+input_num2 = input("Enter 2nd num: ")
+input_num3 = input("Enter 3rd num: ")
+
+print(max_num(int(input_num1), int(input_num2), int(input_num3)))
+
+
+def string_functions(str_color):
+    if str_color == "Blue":
+        print("You hair color is black and nice")
+        return str_color
+    elif str_color == "Red":
+        print("You hair color is Red and you are beautiful")
+        return str_color
+    else:
+        print("You hair color seems normal")
+        return str_color
+
+
+hair_color = input("Input your hair color: ")
+print(string_functions(hair_color))
+
+
+
